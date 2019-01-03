8d1d6914a079038344148f901a810ab6d835984c
Daniel Costea <daniel_costea@ymail.com>
2019-01-04 00:53:26 +0200

    First Commit

---
+++ RouteMappingStaticFiles/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RouteMappingStaticFiles
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapGet("", context => context.Response.WriteAsync("Hello from Routing!"));
+
+            routeBuilder.MapGet("index", async context => {
+                await StaticFiles.ReturnStaticFile(context);
+            });
+
+            app.UseRouter(routeBuilder.Build());
+        }
+    }
+}
---
+++ RouteMappingStaticFiles/StaticFiles.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteMappingStaticFiles
+{
+    public class StaticFiles
+    {
+        public static async Task ReturnStaticFile(HttpContext context)
+        {
+            FileInfo file;
+
+            file = new FileInfo($"wwwroot/index.html");
+
+            byte[] buffer;
+            if (file.Exists)
+            {
+                buffer = File.ReadAllBytes(file.FullName);
+
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "text/html";
+                context.Response.ContentLength = buffer.Length;
+                context.Response.Headers.Add("FileName", file.Name);
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                context.Response.ContentType = "text/plain";
+
+                buffer = Encoding.UTF8.GetBytes("Not Found, 404");
+            }
+
+            using (var stream = context.Response.Body)
+            {
+                await stream.WriteAsync(buffer, 0, buffer.Length);
+                await stream.FlushAsync();
+            }
+        }
+    }
+}
